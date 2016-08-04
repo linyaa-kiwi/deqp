065cd8fb61be03c81453fa3d49e9b1581df432c9
Chad Versace <chad@kiwitree.net>
2016-08-04 15:19:50 -0700

    Move repo
    Repository has moved to
        git://git.kiwitree.net/~chadv/deqp
        branch master
    
    Change-Id: Iae1183c6ff0c9bf7a6ab57264c28783b27bb2511

--- scripts/build/__init__.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-__all__ = ['common', 'config', 'build']
--- targets/android/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Removing old dEQP Ondevice Package..."
-adb $* uninstall com.drawelements.deqp
-
-echo "Installing dEQP Ondevice Package..."
-adb $* install -r dEQP.apk
